86802b58143bcc77a02cdf78695cf53fdbb395f6
bigguy <bigguy@163.com>
2019-06-08 19:21:48 +0800

    增加 Movie-Eureka 相当于是服务的注册中心

---
+++ movie-eureka/src/main/java/com/bigguy/springcloud/EurekaApp.java
@@ -0,0 +1,21 @@
+package com.bigguy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author bigguy_hc
+ * @create 2019-06-08 19:18
+ */
+@EnableEurekaServer                 // 启动Eureka 的注解
+@SpringBootApplication
+public class EurekaApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaApp.class, args);
+
+    }
+
+}
